156ac4e880b89f573aeb3fda78765451c702c312
coaadasd <kimsunghoon122@gmail.com>
2024-01-03 14:42:15 +0900

    tkqkf

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,32 +1,28 @@
-import axios from "axios"
+// todoApi.js
+import axios from "axios";
 
-//서버 주소
-export const API_SERVER_HOST = 'http://172.16.1.196:8097'
+export const API_SERVER_HOST = 'http://172.16.1.196:8097';
+const prefix = `${API_SERVER_HOST}`;
 
-const prefix = `${API_SERVER_HOST}`
+export const getOne = async (id, tno) => {
+  const res = await axios.get(`${prefix}/categories/${id}/contents/${tno}`);
+  return res.data;
+};
 
-export const getOne = async (id,tno) => {
-  const res = await axios.get(`${prefix}/categories/${id}/contents/${tno}` )
+export const getList = async (pageParam) => {
+  const res = await axios.get(`${prefix}/categories/${pageParam.id}/contents`);
+  return res.data;
+};
 
-  return res.data 
-}
-
-export const getList = async ( pageParam ) => {
-  const res = await axios.get(`${prefix}/categories/${pageParam.id}/contents`)
-  return res.data
-}
-
-export const postAdd = async ( pageParam,todo ) => {
-  console.log(todo)
-  const res = await axios.post(`${prefix}/categories/${pageParam}/contents/add` , {
-    title:todo.title,
-    writer:todo.writer,
-    content:todo.content,
-    pw:todo.pw
-  })
-
-  return res.data
-} 
+export const postAdd = async (pageParam, todo) => {
+  const res = await axios.post(`${prefix}/categories/${pageParam}/contents/add`, {
+    title: todo.title,
+    writer: todo.writer,
+    content: todo.content,
+    pw: todo.pw,
+  });
+  return res.data;
+};
 
 export const deleteOne = async (pwData, tno, pageParam) => {
   try {
@@ -36,11 +32,45 @@ export const deleteOne = async (pwData, tno, pageParam) => {
     console.error("deleteOne error:", error);
     throw error;
   }
-}
+};
 
-export const putOne = async (todo, pageParam ,tno) => {
-  const res = await axios.put(`${prefix}/categories/${pageParam}/contents/${tno}`, todo)
+export const putOne = async (todo, pageParam, tno) => {
+  const res = await axios.put(`${prefix}/categories/${pageParam}/contents/${tno}`, todo);
+  return res.data;
+};
 
-  return res.data
-}
+export const getComments = async (id, tno) => {
+  try {
+    const res = await axios.get(`${prefix}/categories/${id}/contents/${tno}/comments`);
+    console.log('Server Response:', res.data); // 추가
+    return res.data || [];
+  } catch (error) {
+    console.error('Error getting comments:', error);
+    throw error;
+  }
+};
 
+export const createComment = async (id, tno, commentData) => {
+  const res = await axios.post(`${prefix}/categories/${id}/contents/${tno}/comments`, {
+    title: commentData.title,
+    writer: commentData.writer,
+    content: commentData.content,
+    pw: commentData.pw,
+  });
+  return res.data;
+};
+
+export const updateComment = async (id, tno, commentId, updatedCommentData) => {
+  const res = await axios.post(`${prefix}/categories/${id}/contents/${tno}/comments/${commentId}`, updatedCommentData);
+  return res.data;
+};
+
+export const deleteComment = async (id, tno, commentId, pwData) => {
+  try {
+    const res = await axios.post(`${prefix}/categories/${id}/contents/${tno}/comments/${commentId}`, pwData);
+    return res.data;
+  } catch (error) {
+    console.error("deleteComment error:", error);
+    throw error;
+  }
+};
--- src/components/todo/ReadComponent.js
+++ src/components/todo/ReadComponent.js
@@ -1,48 +1,140 @@
-  import { useEffect, useState } from "react"
-  import { useParams } from "react-router-dom"
-  import  {getOne} from "../../api/todoApi"
-  import useCustomMove from "../hooks/useCustomMove"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getOne, getComments, createComment, deleteComment } from "../../api/todoApi";
+import useCustomMove from "../hooks/useCustomMove";
 
-  const initState = {
-    tno:0,
-    title:'',
+const initState = {
+  tno: 0,
+  title: '',
+  writer: '',
+  content: '',
+  password: ''
+}
+
+const ReadComponent = () => {
+  const { id, tno } = useParams();
+  const [todo, setTodo] = useState(initState);
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState({
+    title: '',
     writer: '',
     content: '',
-    password: ''
-  }
+    pw: ''
+  });
 
-  const ReadComponent = () => {
-    const {id,tno} = useParams();
-    const [todo, setTodo] = useState(initState) //아직 todo는 사용하지 않음 
-    
-    const {moveToList, moveToModify} = useCustomMove()  
+  const { moveToList, moveToModify } = useCustomMove();
 
-    useEffect(() => {
-      getOne(id,tno).then(data => {
-        console.log(data)
-        setTodo(data.data)
-      })    
-    }, [tno])
+  useEffect(() => {
+    getOne(id, tno).then(data => {
+      setTodo(data.data);
+    });
 
-    return (  
-    <div className = "border-2 border-sky-200 mt-10 m-2 p-4 ">
-        
+    getComments(id, tno).then(commentData => {
+      console.log('Comments Data:', commentData); // 콘솔 출력
+      setComments(commentData);
+    });
+  }, [id, tno]);
+
+  const addComment = async () => {
+    try {
+      console.log('New Comment Data:', newComment); // 콘솔 출력
+      const response = await createComment(id, tno, newComment);
+      setComments(prevComments => [...prevComments, response]);
+      setNewComment({
+        title: '',
+        writer: '',
+        content: '',
+        pw: ''
+      });
+    } catch (error) {
+      console.error('Error adding comment:', error);
+    }
+  };
+
+  const removeComment = async (commentId) => {
+    console.log('Removing Comment with ID:', commentId); // 콘솔 출력
+    try {
+      await deleteComment(id, tno, commentId, { pw: newComment.pw });
+      setComments(prevComments => prevComments.filter(comment => comment.id !== commentId));
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+    }
+  };
+
+  const makeDiv = (title, value) => (
+    <div className="flex justify-center">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+        <div className="w-1/5 p-6 text-right font-bold">{title}</div>
+        <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">{value}</div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="border-2 border-sky-200 mt-10 m-2 p-4">
       {makeDiv('Writer', todo.writer)}
       {makeDiv('Title', todo.title)}
       {makeDiv('Content', todo.content)}
       {makeDiv('Password', todo.password)}
 
-      {/* buttons.........start */}
-      <div className="flex justify-end p-4">
+      <div className="mt-4">
+        <h2 className="text-lg font-semibold">댓글 목록</h2>
+        <ul>
+        {comments.map(comment => (
+          <li key={comment.id}>
+            {JSON.stringify(comment)}{' '}
+            <button onClick={() => removeComment(comment.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+      </div>
 
-        <button type="button" 
+      <div className="mt-4">
+        <div>
+          <input
+            type="text"
+            placeholder="댓글 제목"
+            value={newComment.title}
+            onChange={(e) => setNewComment(prev => ({ ...prev, title: e.target.value }))}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="댓글 작성자"
+            value={newComment.writer}
+            onChange={(e) => setNewComment(prev => ({ ...prev, writer: e.target.value }))}
+          />
+        </div>
+        <div>
+          <textarea
+            placeholder="댓글 내용"
+            value={newComment.content}
+            onChange={(e) => setNewComment(prev => ({ ...prev, content: e.target.value }))}
+          />    
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="댓글 비밀번호"
+            value={newComment.pw}
+            onChange={(e) => setNewComment(prev => ({ ...prev, pw: e.target.value }))}
+          />
+        </div>
+        <button onClick={addComment}>댓글 작성</button>
+      </div>
+
+      <div className="flex justify-end p-4">
+        <button
+          type="button"
           className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
           onClick={() => moveToList()}
         >
           List
         </button>
 
-        <button type="button" 
+        <button
+          type="button"
           className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500"
           onClick={() => moveToModify(tno)}
         >
@@ -50,16 +142,7 @@
         </button>
       </div>
     </div>
-    )
-  }
+  );
+};
 
-  const makeDiv = (title,value) =>       
-  <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-      <div className="w-1/5 p-6 text-right font-bold">{title}</div>
-      <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">{value}</div>
-    </div>
-  </div>
-
-  
-  export default ReadComponent
+export default ReadComponent;
